29b8033d5cc053f87db5207751407b1c898a4dd2
dinukasaminda <dinukasaminda@gmail.com>
2016-11-03 22:35:20 +0530

    add string value

---
+++ app/src/main/java/com/mapapitest/dinukasbandara/myapplication/testgitjava.java
@@ -0,0 +1,11 @@
+package com.mapapitest.dinukasbandara.myapplication;
+
+/**
+ * Created by Dinuka S Bandara on 11/3/2016.
+ */
+
+public class testgitjava {
+    public static void main(String[] args) {
+
+    }
+}
